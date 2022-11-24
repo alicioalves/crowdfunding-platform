49813a64e408371b0247cdbc1f940d527e888fcd
Alicio <alicio.alvesm@gmail.com>
2022-11-24 02:05:59 -0300

    feat: first steps of crowdfunding project creation

--- src/components/CreateProject.vue
+++ src/components/CreateProject.vue
@@ -1,39 +1,89 @@
 <template>
   <div class="flex flex-col">
     <h1>Bring your project to life</h1>
-    <form action="submit">
+    <div class="flex flex-col">
+      <label for="title">Title</label>
+      <input type="text" name="title" v-model="newProject.title" />
+    </div>
+
+    <div class="flex flex-col">
+      <label for="description">Description</label>
+      <textarea
+        name="description"
+        cols="30"
+        rows="10"
+        v-model="newProject.description"
+      ></textarea>
+    </div>
+
+    <div class="flex justify-center gap-2">
       <div class="flex flex-col">
-        <label for="title">Title</label>
-        <input type="text" name="title" />
+        <label for="amount">Amount Needed (ETH)</label>
+        <input type="number" name="amount" v-model="newProject.amountGoal" />
       </div>
 
       <div class="flex flex-col">
-        <label for="description">Description</label>
-        <textarea name="description" cols="30" rows="10"></textarea>
+        <label for="duration">Duration (in days)</label>
+        <input
+          type="number"
+          name="duration"
+          v-model="newProject.duration"
+          @click="startProject()"
+        />
       </div>
+    </div>
 
-      <div class="flex justify-center gap-2">
-        <div class="flex flex-col">
-          <label for="amount">Amount Needed (ETH)</label>
-          <input type="number" name="amount" />
-        </div>
-
-        <div class="flex flex-col">
-          <label for="duration">Duration (in days)</label>
-          <input type="number" name="duration" />
-        </div>
-      </div>
-
-      <div class="flex justify-around">
-        <button type="button">CLOSE</button>
-        <button type="submit">SAVE</button>
-      </div>
-    </form>
+    <div class="flex justify-around">
+      <button>CLOSE</button>
+      <button>SAVE</button>
+    </div>
   </div>
 </template>
 
 <script>
+import crowdfundInstance from '../../contracts/crowdfundInstance'
+import crowdfundProject from '../../contracts/crowdfundProjectInstance'
+import web3 from '../../contracts/web3'
+
 export default {
-  name: 'CreateProject'
+  name: 'CreateProject',
+  data() {
+    return {
+      startProjectDialog: false,
+      account: null,
+      projectData: [],
+      newProject: { isLoading: false }
+    }
+  },
+
+  methods: {
+    startProject() {
+      this.newProject.isLoading = true
+      crowdfundInstance.methods
+        .startProject(
+          this.newProject.title,
+          this.newProject.description,
+          this.newProject.duration,
+          web3.utils.toWei(this.newProject.amountGoal, 'ether')
+        )
+        .send({
+          from: this.account
+        })
+        .then((res) => {
+          const projectInfo = res.events.ProjectStarted.returnValues
+          projectInfo.isLoading = false
+          projectInfo.currentAmount = 0
+          projectInfo.currentState = 0
+          projectInfo.contract = crowdfundProject(projectInfo.contractAddress)
+          this.startProjectDialog = false
+          this.newProject = { isLoading: false }
+        })
+    }
+  },
+  mounted() {
+    web3.eth.getAccounts().then((accounts) => {
+      [this.account] = accounts
+    })
+  }
 }
 </script>
