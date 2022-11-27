76fcb9a57935ad8c12ecbf6a24c7e588b62fe43f
Alicio <alicio.alvesm@gmail.com>
2022-11-27 02:13:54 -0300

    feat: CreateProject is now a modal

--- src/components/CreateProject.vue
+++ src/components/CreateProject.vue
@@ -1,36 +1,85 @@
 <template>
-  <div class="flex flex-col">
-    <h1>Bring your project to life</h1>
-    <div class="flex flex-col">
-      <label for="title">Title</label>
-      <input type="text" name="title" v-model="newProject.title" />
-    </div>
+  <div
+    class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex bg-black bg-opacity-40"
+  >
+    <div class="relative w-auto my-6 mx-auto max-w-sm">
+      <div
+        class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+      >
+        <div
+          class="flex items-center justify-center p-5 border-b border-solid border-slate-200 rounded-t"
+        >
+          <h1 class="font-bold text-xl">Bring your project to life</h1>
+          <button
+            class="ml-auto border-0 text-black float-right text-3xl leading-none"
+            @click="closeModal()"
+          >
+            <span class="text-black text-2xl block"> x </span>
+          </button>
+        </div>
+        <div class="flex flex-col justify-center p-5">
+          <div class="flex flex-col">
+            <label for="title">Title</label>
+            <input
+              class="border border-black"
+              type="text"
+              name="title"
+              v-model="newProject.title"
+            />
+          </div>
 
-    <div class="flex flex-col">
-      <label for="description">Description</label>
-      <textarea
-        name="description"
-        cols="30"
-        rows="10"
-        v-model="newProject.description"
-      ></textarea>
-    </div>
+          <div class="flex flex-col">
+            <label for="description">Description</label>
+            <textarea
+              class="border border-black"
+              name="description"
+              cols="30"
+              rows="10"
+              v-model="newProject.description"
+            ></textarea>
+          </div>
 
-    <div class="flex justify-center gap-2">
-      <div class="flex flex-col">
-        <label for="amount">Amount Needed (ETH)</label>
-        <input type="number" name="amount" v-model="newProject.amountGoal" />
+          <div class="flex flex-col justify-center gap-2">
+            <div class="flex flex-col">
+              <label for="amount">Amount Needed (ETH)</label>
+              <input
+                class="border border-black"
+                type="number"
+                name="amount"
+                v-model="newProject.amountGoal"
+              />
+            </div>
+
+            <div class="flex flex-col">
+              <label for="duration">Duration (in days)</label>
+              <input
+                class="border border-black"
+                type="number"
+                name="duration"
+                v-model="newProject.duration"
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b"
+        >
+          <button
+            class="text-black bg-transparent border border-solid border-black hover:bg-black hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            @click="closeModal()"
+          >
+            Close
+          </button>
+          <button
+            class="text-black bg-transparent border border-solid border-black hover:bg-black hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            @click="startProject()"
+          >
+            Create Project
+          </button>
+        </div>
       </div>
-
-      <div class="flex flex-col">
-        <label for="duration">Duration (in days)</label>
-        <input type="number" name="duration" v-model="newProject.duration" />
-      </div>
-    </div>
-
-    <div class="flex justify-around">
-      <button>CLOSE</button>
-      <button @click="startProject()">SAVE</button>
     </div>
   </div>
 </template>
@@ -53,6 +102,7 @@ export default {
 
   methods: {
     startProject() {
+      this.closeModal()
       this.newProject.isLoading = true
       crowdfundInstance.methods
         .startProject(
@@ -70,14 +120,17 @@ export default {
           projectInfo.currentAmount = 0
           projectInfo.currentState = 0
           projectInfo.contract = crowdfundProject(projectInfo.contractAddress)
-          this.startProjectDialog = false
           this.newProject = { isLoading: false }
         })
+    },
+
+    closeModal() {
+      this.$emit('close')
     }
   },
   mounted() {
     web3.eth.getAccounts().then((accounts) => {
-      [this.account] = accounts
+      ;[this.account] = accounts
     })
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,18 @@
       <p class="">Utilizing Ethereum for Decentralized Crowdfunding</p>
     </div>
 
-    <button>Start a Project</button>
+    <button
+      @click="shouldDisplayCreateProjectModal = true"
+      type="button"
+      class="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+    >
+      Start a Project
+    </button>
 
-    <CreateProject />
+    <CreateProject
+      v-if="shouldDisplayCreateProjectModal"
+      @close="shouldDisplayCreateProjectModal = false"
+    />
   </div>
 </template>
 
@@ -18,6 +27,12 @@ export default {
   name: 'HomeView',
   components: {
     CreateProject
+  },
+
+  data() {
+    return {
+      shouldDisplayCreateProjectModal: false
+    }
   }
 }
 </script>
