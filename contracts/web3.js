a3129550500294bcb186f48bf6301bebb8db0dbc
Alicio <alicio.alvesm@gmail.com>
2022-11-27 18:43:27 -0300

    fix: updated wallet sign in

--- contracts/web3.js
+++ contracts/web3.js
@@ -4,8 +4,8 @@ const Web3 = require('web3')
 const web3 = new Web3(ethereum || 'ws://localhost:8546')
 
 try {
-  // Request account access if needed
-  ethereum.enable()
+  // Request account access from a application like Status, Mist or Metamask if needed
+  web3.eth.requestAccounts()
 } catch (error) {
   console.log(error)
 }
